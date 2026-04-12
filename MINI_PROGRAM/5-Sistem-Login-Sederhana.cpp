110533b261466b99992cd392f7c521f3c63e7d70
vandhaffa07 <muhammaddhaffa500@gmail.com>
2026-04-12 14:54:11 +0700

    Update 5-Sistem-Login-Sederhana.cpp

--- MINI_PROGRAM/5-Sistem-Login-Sederhana.cpp
+++ MINI_PROGRAM/5-Sistem-Login-Sederhana.cpp
@@ -2,41 +2,81 @@
 using namespace std;
 
 int main(){
+  char lagi;
 
-        string usn_admin = "Admin123";
-        string pw_admin = "321Admin";
-        string usn_admin_input, pw_admin_input;
-        int identitas;
+  string usn_admin = "Admin123";
+  string pw_admin = "321Admin";
+  string usn_akun_user, pw_akun_user;
+  bool akun = false;
+
+  do {
+  int pilihan;
 
         cout << "===SELAMAT DATANG DI SISTEM LOGIN===\n\nMAU LOGIN SEBAGAI SIAPA?" << endl;
-        cout << "1. ADMIN\n2. USER" << endl;
-        cout << "KETIKKAN JAWABANMU ( 1 , 2 ) : ";
-        cin >> identitas;
+        cout << "1. ADMIN\n2. LOGIN USER\n3. DAFTAR USER" << endl;
+        cout << "KETIKKAN JAWABANMU ( 1, 2, 3 ) : ";
+        while ( !(cin >> pilihan) || pilihan < 1 || pilihan > 3 || cin.peek() != '\n' ){
+          cout <<"MASUKKAN JAWABAN YANG VALID (1, 2, 3) : ";
+          cin.clear();
+          cin.ignore(1000, '\n');
+        }
         cout<<endl;
 
-        if ( identitas == 1 ){
-                cout << "MASUKKAN USERNAME : ";
-                cin >> usn_admin_input;
+	switch ( pilihan ){
+        case 1 : {
+	cin.ignore(1000, '\n');
+        string usn_admin_input, pw_admin_input;
+          cout << "MASUKKAN USERNAME : ";
+           getline(cin, usn_admin_input);
 
-                cout << "MASUKKAN PASSWORD : ";
-                cin >> pw_admin_input;
+          cout << "MASUKKAN PASSWORD : ";
+           getline(cin, pw_admin_input);
 
-                if ( (usn_admin_input==usn_admin) && (pw_admin_input==pw_admin) ){
-                                cout << "AKSES DITERIMA, SELAMAT DATANG KEMBALI"<< endl;
-                } else {
-                                cout << "AKSES DITOLAK, USERNAME ATAU PASSWORD SALAH" << endl;
-                }
-        } else if ( identitas == 2 ){
-                string usn_daftar, pw_daftar;
-
-                        cout << "ANDA BELUM PUNYA AKUN, SILAHKAN DAFTAR TERLEBIH DAHULU" << endl;
-                        cout << "MASUKKAN USERNAME : ";
-                        cin >> usn_daftar;
-                        cout << "MASUKKAN PASSWORD : ";
-      cin >> pw_daftar;
-                        cout << "\nSELAMAT DATANG " << usn_daftar << endl;
-        } else {
-                cout << "HARAP MASUKKAN JAWABAN YANG VALID" << endl;
+          if ( (usn_admin_input==usn_admin) && (pw_admin_input==pw_admin) ){
+            cout << "AKSES DITERIMA, LOGIN SEBAGAI ADMIN BERHASIL, SELAMAT DATANG"<< endl;
+          } else {
+            cout << "AKSES DITOLAK, LOGIN ADMIN GAGAL, USERNAME ATAU PASSWORD SALAH" << endl;
+          }
+	break;
         }
-        return 0;
+
+	case 2 : {
+	cin.ignore(1000, '\n');
+        string usn_login_user, pw_login_user;
+	  if ( !akun ){
+	    cout << "ANDA BELUM PUNYA AKUN, SILAHKAN DAFTAR TERLEBIH DAHULU" << endl;
+	    break;
+	  }
+	  cout << "MASUKKAN USERNAME : ";
+	    getline(cin, usn_login_user);
+	  cout << "MASUKKAN PASSWORD : ";
+	    getline(cin, pw_login_user);
+	    if ( (usn_login_user == usn_akun_user) && (pw_login_user == pw_akun_user) ){
+	      cout << "LOGIN SEBAGAI USER BERHASIL, SELAMAT DATANG " << usn_akun_user << endl;
+	    } else {
+	      cout << "LOGIN USER GAGAL, USERNAME ATAU PASSWORD SALAH" << endl;
+	    }
+	break;
+	}
+
+	case 3 : {
+	cin.ignore(1000, '\n');
+	string usn_daftar_user, pw_daftar_user;
+	  cout << "MASUKKAN USERNAME : ";
+	    getline(cin, usn_daftar_user);
+	  cout << "MASUKKAN PASSWORD : ";
+	    getline(cin, pw_daftar_user);
+	  akun = true;
+	  usn_akun_user = usn_daftar_user;
+	  pw_akun_user = pw_daftar_user;
+	  cout << "PENDAFTARAN BERHASIL" << endl;
+	break;
+	}
+	}
+
+  cout << "\nKembali ke menu? (y/n): ";
+  cin >> lagi;
+} while (lagi == 'y' || lagi == 'Y');
+
+return 0;
 }
